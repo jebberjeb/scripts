45327dc668bd2c0a422c482087aafd2154cadc68
Jeb Beich <jebbeich@gmail.com>
2014-09-24 10:50:36 -0400

    added -y to yum commands

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo yum install ack
+sudo yum -y install ack
 vim +PluginInstall +qall
 cd ~/.vim/bundle/vimproc.vim
 make
