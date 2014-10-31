5b163019f7a4c2f6b37d9fc9cb961dc57a28e13d
Jeb Beich <jebbeich@gmail.com>
2014-10-31 01:08:41 -0500

    exorted script to create symlinks for dotfiles

---
+++ symlinks.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+ln -s ~/source/dotfiles/.bashrc ~/.bashrc
+ln -s ~/source/dotfiles/.bashrc-git ~/.bashrc-git
+ln -s ~/source/dotfiles/.bashrc-path ~/.bashrc-path
+ln -s ~/source/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/source/dotfiles/.inputrc ~/.inputrc
+ln -s ~/source/dotfiles/.vimrc ~/.vimrc
+ln -s ~/source/dotfiles/.vim ~/.vim
+ln -s ~/source/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/source/dotfiles/.lein ~/.lein
+ln -s ~/source/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/source/dotfiles/.gitignore ~/.gitignore
+ln -s ~/source/dotfiles/.gitconfig ~/.gitconfig
+
