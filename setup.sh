a452d77454f26535a1b76c98830651bf9298b736
Jeb Beich <jebbeich@gmail.com>
2014-12-11 11:14:12 -0600

    add debug output

--- setup.sh
+++ setup.sh
@@ -10,29 +10,41 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Turn off SELinux
+echo "Disabling SE Linux"
 echo "SELINUX=disabled
 SELINUXTYPE=targeted" | sudo tee /etc/selinux/config
 
 # Grab env script, set os, installer
+echo "Set ENV"
 curl https://raw.githubusercontent.com/jebberjeb/scripts/master/env.sh > env.sh
 . env.sh
 set_os
 
+# Prime Installer
+echo "Prime Installer"
+if [[ "$os" == "ubuntu" ]]; then
+    sudo $installer update
+fi
+
 # Set Central timezone
+echo "Set timezone"
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
 
 # Turn on audio
+echo "Configure audio"
 #sudo adduser jeb audio
 sudo usermod -a -G audio jeb
 
 # Only need to disable gui boot on Fedora, since we're using
 # Ubuntu server.
+echo "Set run-level 3"
 if [[ "$os" == "fedora" ]]; then
     sudo systemctl set-default multi-user.target
 fi
 
 # Install packages
+echo "Install common packages"
 sudo $installer install python
 sudo $installer install ruby
 sudo $installer install git
@@ -43,6 +55,7 @@ sudo $installer install subversion
 
 if [[ "$os" == "ubuntu" ]]; then
 
+    echo "Install Ubuntu packages"
     sudo $installer install xfce4 xfce4-goodies
     sudo $installer install gnome-icon-theme-full tango-icon-theme
     sudo $installer install make
@@ -55,6 +68,7 @@ if [[ "$os" == "ubuntu" ]]; then
 
 elif [[ "$os" == "fedora" ]]; then
 
+    echo "Install Fedora packages"
     sudo $installer install @xfce
     sudo $installer groupinstall "Development Tools"
     sudo $installer install java
@@ -66,6 +80,7 @@ elif [[ "$os" == "fedora" ]]; then
 fi
 
 # dotfiles
+echo "Setup dotfiles and scripts"
 mkdir ~/source
 cd ~/source
 git clone https://github.com/jebberjeb/scripts
@@ -79,6 +94,7 @@ rm .bash_profile
 ~/source/scripts/symlinks.sh
 
 # Chrome
+echo "Install Chrome"
 if [[ "$os" == "ubuntu" ]]; then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
@@ -90,6 +106,7 @@ elif [[ "$os" == "fedora" ]]; then
 fi
 
 # Maven
+echo "Install Maven"
 wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 tar xvf apache-maven-3.0.5-bin.tar.gz
 sudo cp -rf apache-maven-3.0.5 /usr/local/
@@ -97,10 +114,12 @@ rm -rf apache-maven-3.0.5
 rm apache-maven-3.0.5-bin.tar.gz
 
 # Leiningen
+echo "Install Leiningen"
 curl --insecure https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein | sudo tee /usr/local/bin/lein
 sudo chmod a+x /usr/local/bin/lein
 
 # Hipchat
+echo "Install HipChat"
 if [[ "$os" == "ubuntu" ]]; then
     sudo su
     echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
@@ -121,6 +140,7 @@ gpgkey=https://www.hipchat.com/keys/hipchat-linux.key
 fi
 
 # BitchX
+echo "Install BitchX"
 cd ~
 svn checkout svn://svn.code.sf.net/p/bitchx/code/trunk bitchx-code
 cd bitchx-code
@@ -129,6 +149,7 @@ make
 sudo make install
 
 # DragonDisk (for s3)
+echo "Install DragonDisk"
 if [[ "$os" == "ubuntu" ]]; then
     wget http://download.dragondisk.com/dragondisk_1.0.5-0ubuntu_amd64.deb
     sudo $installer install libqt4-dbus libqt4-network libqt4-xml libqtcore4 libqtgui4
@@ -140,6 +161,7 @@ elif [[ "$os" == "fedora" ]]; then
 fi
 
 # Vim
+echo "Install Vim"
 cd ~
 git clone https://github.com/b4winckler/vim.git ~/vim
 cd ~/vim
@@ -151,6 +173,7 @@ sudo make install
 ~/source/scripts/vim-setup.sh
 
 # Finalize config(s)
+echo "Finalize config"
 cd ~/source/scripts
 ./spit-xfce-config.sh
 
