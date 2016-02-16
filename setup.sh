c8b8758f70a423251a6a648f0d90cda769f2a236
Jeb Beich <jebbeich@gmail.com>
2016-02-16 16:05:51 -0600

    added tree package

--- setup.sh
+++ setup.sh
@@ -48,6 +48,7 @@ function install_packages() {
     sudo apt-get -y install libreoffice
     sudo apt-get -y install htop
     sudo apt-get -y install rlwrap
+    sudo apt-get -y install tree
 }
 
 function setup_dotfiles() {
